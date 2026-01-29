6ecb33e0a4d94d91f9c8bb46c2bd798397377018
llmnark <ignacykazicki@gmail.com>
2026-01-29 23:25:14 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,24 +3,25 @@ const img2 = document.getElementById("s2img2");
 const img3 = document.getElementById("s2img3");
 
 img1.addEventListener('mouseenter', () => {
-    img1.src = "../img/digits_img_t.jpg";
+    img1.src = "img/digits_img_t.jpg";
 });
 img1.addEventListener('mouseleave', () => {
-    img1.src = "../img/digits_img.jpg";
+    img1.src = "img/digits_img.jpg";
 });
 
 img2.addEventListener('mouseenter', () => {
-    img2.src = "../img/no_digits_img_t.jpg";
+    img2.src = "img/no_digits_img_t.jpg";
 });
 img2.addEventListener('mouseleave', () => {
-    img2.src = "../img/no_digits_img.jpg";
+    img2.src = "img/no_digits_img.jpg";
 });
 
 img3.addEventListener('mouseenter', () => {
-    img3.src = "../img/flagged_tile_t.jpg";
+    img3.src = "img/flagged_tile_t.jpg";
 });
 img3.addEventListener('mouseleave', () => {
-    img3.src = "../img/flagged_tile.jpg";
+    img3.src = "img/flagged_tile.jpg";
 
 });
+
 
